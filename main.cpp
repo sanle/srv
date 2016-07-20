728833def4889f56d4445b3e37737fbbdb755249
lebedinskiy@mail.ua <lebedinskiy@findir.dn.aska>
2016-07-20 20:39:30 +0300

    fix 404

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ void process_list(int sock,std::string path, std::vector<std::string> &list)
 	file = match[1];
 	if(file.length() == 1)
 	{
-		std::string message = "HTTP/1.0 400 Bad Request\r\n\r\n";
+		std::string message = "HTTP/1.0 404 Not Found\r\n\r\n";
 		send(sock,message.c_str(),message.length(),0);
 		return;
 	}
